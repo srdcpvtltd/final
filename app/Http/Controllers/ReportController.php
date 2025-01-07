12f010646c7dc991490f423544cceeca4849f2bf
srdcpvtltd <debendraster@gmail.com>
2025-01-07 18:29:30 +0530

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -520,13 +520,14 @@ class ReportController extends Controller
         }
         $bookings = $sql->paginate(20);
         $countries = \DB::table('countries')->get();
+        $police_stations = \DB::table('police_stations')->get();
         if ($request->get('police_station') != '') {
 	        $hotelProfileIds = HotelProfile::where('police_station', $request->get('police_station'))->pluck('id');
             $sql->whereIn('hotel_id', $hotelProfileIds);
             $inputs['police_station'] = $request->get('police_station');
             }
        $ageArr = \DB::table('bookings')->groupBy('age')->orderBy('age', 'ASC')->pluck('age');
-       return view('admin.report.simpleReport', compact('countries', 'bookings', 'inputs', 'states', 'cities', 'police_station', 'ageArr'));
+       return view('admin.report.simpleReport', compact('countries', 'bookings', 'inputs', 'states', 'cities', 'police_stations', 'ageArr'));
     }
 
     public function adminexport(Request $request)
